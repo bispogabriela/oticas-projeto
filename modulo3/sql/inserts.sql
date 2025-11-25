f84cdad591704e73101fdadddce5e56a6d246333
bispogabriela <bispo.gabriela@ufms.br>
2025-11-24 21:10:15 -0400

    Create inserts.sql

---
+++ modulo3/sql/inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO Categoria (nome_categoria) VALUES 
+('Óculos de Sol'),
+('Óculos de Grau');
+
+INSERT INTO Produto (codigo_produto, nome, preco_unitario, quantidade_estoque, quantidade_minima, categoria_id)
+VALUES
+('10186', 'ARMANI EX SOLAR 0AX4145S-839980-57', 141.90, 20, 5, 1),
+('10192', 'ARMANI RX 0AX1073-6000-56', 132.73, 15, 5, 2),
+('10194', 'ARMANI RX 0AX1073-6003-56', 132.73, 10, 5, 2);
